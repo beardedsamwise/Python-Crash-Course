1b513ac699d055ba07e6705be6f1f8b3bad46fc0
Sam Bentley <samjamesbentley@gmail.com>
2019-04-17 16:50:06 +1000

    finished chapter 4

---
+++ Chapter 4/tuples.py
@@ -0,0 +1,11 @@
+buffet = ('pizza','pasta','stir fry','dumplings','cake')
+
+for food in buffet:
+	print(food)
+
+buffet = ('pizza','pasta','ramen','gyoza','cake')
+
+print("\n")
+
+for food in buffet:
+	print(food)

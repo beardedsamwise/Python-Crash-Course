242d697e0d6efc2e38d91e66baf0c7c22791d4ad
Sam Bentley <samjamesbentley@gmail.com>
2019-04-23 17:23:51 +1000

    Chapter 5 work

---
+++ Chapter 5/conditional_tests_1.py
@@ -0,0 +1,16 @@
+shoes = 'La Sportiva'
+
+print("Are shoes == 'La Sportiva'? I predict True")
+print(shoes == 'La Sportiva')
+
+print("\nAre shoes == 'Nike'? I predict False")
+print(shoes == 'Nike')
+
+print("\nAre shoes != 'Nike'? I predict True")
+print(shoes != 'Nike')
+
+print("\nAre shoes != 'la sportiva'? I predict True")
+print(shoes != 'la sportiva')
+
+print("\nAre shoes == 'la sportiva' if we ignore the case? I predict True")
+print(shoes.lower() == 'la sportiva')

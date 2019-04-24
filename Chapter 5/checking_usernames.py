c6adb44143ef8c38954837647010a78d8df48423
Sam Bentley <samjamesbentley@gmail.com>
2019-04-24 19:44:42 +1000

    finish chapter 5

--- Chapter 5/checking_usernames.py
+++ Chapter 5/checking_usernames.py
@@ -1,9 +1,12 @@
 current_users = ['peter','alex','john','steve','dave']
 
-new_users = ['ralph','Alex','michael','stewart','reginald']
+new_users = ['Ralph','Alex','Michael','Stewart','Reginald']
 
 #finish this off
 current_users_lower = []
+
+for users in current_users:
+	current_users_lower.append(users.lower())
 
 for new_user in new_users:
 	if new_user.lower() in current_users_lower:

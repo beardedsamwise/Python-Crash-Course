e70c4b32133b76656fc4249733cdda7a2ccc9e76
Sam Bentley <samjamesbentley@gmail.com>
2019-04-28 12:41:54 +1000

    work on chapter 6

--- Chapter 6/glossary.py
+++ Chapter 6/glossary.py
@@ -1,9 +1,9 @@
 programming_words = {
 	'list' : 'A list is a collection of items in a particular order',
-	'loop' : 'Looping allows you to take the same action, or a set of , ' \
+	'loop' : 'Looping allows you to take the same action, or a set of ' \
 	'actions with every item in a list',
 	'tuple' : 'A tuple is a list with immutable items',
-	'if statement' : 'An if statement allows you to examine and respond , ' \
+	'if statement' : 'An if statement allows you to examine and respond' \
 	' to the state of a program',
 	}
 

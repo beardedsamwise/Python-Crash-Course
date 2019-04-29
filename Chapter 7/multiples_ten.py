0c89edf07015dedacd3709d0dc3d298000dd712f
Sam Bentley <samjamesbentley@gmail.com>
2019-04-29 16:47:30 +1000

    work on chapter 7

---
+++ Chapter 7/multiples_ten.py
@@ -0,0 +1,8 @@
+number = input("Please enter a number to test if it's divisible by 10: ")
+
+number = int(number)
+
+if number % 10 == 0:
+	print("Your number is divisible by 10!")
+else:
+	print("Your number is not divisible by 10 :(")
---
+++ Chapter 7/restaurant_seating.py
@@ -0,0 +1,8 @@
+num_seats = input("How many people are dining tonight? ")
+
+num_seats = int(num_seats)
+
+if num_seats > 8:
+	print("Please wait, you have a rather large group")
+else:
+	print("Follow me! We have some smaller tables this way.")

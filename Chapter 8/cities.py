f02ed9ed386ab729d7483e423675f3633b32ee0f
Sam Bentley <samjamesbentley@gmail.com>
2019-05-06 17:29:23 +1000

    work on chapter 8

---
+++ Chapter 8/cities.py
@@ -0,0 +1,6 @@
+def describe_city(city,country='Australia'):
+	print(city.title() + " is in " + country.title() + ".")
+
+describe_city('Melbourne')
+describe_city('Tokyo','Japan')
+describe_city('Adelaide')
--- Chapter 8/make_tshirt.py
+++ Chapter 8/make_tshirt.py
@@ -1,6 +1,7 @@
-def make_shirt(size,message):
+def make_shirt(size='large',message='I Love Python'):
 	print("You have ordered a " + size.lower() + " shirt.")
-	print("Your shirt has this message printed on it: " + message.title() + "\n")
+	print("Your shirt has this message printed on it: " + message + "\n")
 
-make_shirt('large','epic winrar')
-make_shirt(size='medium',message='pwned')
+make_shirt()
+make_shirt('medium')
+make_shirt(message='w00t',size='x-large')

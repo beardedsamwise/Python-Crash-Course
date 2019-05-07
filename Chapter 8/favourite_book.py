25e58ed4009a18af07411726bf7d2242859b111d
Sam Bentley <samjamesbentley@gmail.com>
2019-05-08 08:16:59 +1000

    work on chapter 8

--- Chapter 8/favourite_book.py
+++ Chapter 8/favourite_book.py
@@ -1,4 +1,6 @@
 def favourite_book(title):
+	"""This function will list your favourite book in a neatly
+	formatted manner"""
 	print("One of my favourite books is " + title.title() + ".")
 
 favourite_book("name of the wind")
--- Chapter 8/make_tshirt.py
+++ Chapter 8/make_tshirt.py
@@ -1,6 +1,4 @@
-def make_shirt(size='large',message='I Love Python'):
-	print("You have ordered a " + size.lower() + " shirt.")
-	print("Your shirt has this message printed on it: " + message + "\n")
+from make_tshirt_function import make_shirt 
 
 make_shirt()
 make_shirt('medium')

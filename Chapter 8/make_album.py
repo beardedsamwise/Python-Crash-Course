6402cc4ad1c3231d6ef00fbba0813ac71f5a4d5a
Sam Bentley <samjamesbentley@gmail.com>
2019-05-07 19:36:02 +1000

    work on chapter 8

--- Chapter 8/make_album.py
+++ Chapter 8/make_album.py
@@ -1,16 +1,32 @@
 def make_album(artist,album,tracks=''):
-	"""Return a dictionary containing an album and it's associated artist"""
+	"""Return a dictionary containing details about an album"""
 	album_dict = {'artist': artist.title(), 'album': album.title()}
 	if tracks:
 		album_dict['tracks'] = tracks
 	return album_dict
 
-album = make_album('tool','lateralus')
-print(album)	
+while True:
+	print('Please tell me an album: ')
+	print('(enter "q" to quit at any time')
 
-album = make_album('a perfect circle','mer de noms')
-print(album)	
+	artist_name = input('Album name: ')
+	if artist_name == 'q':
+		break
 
-album = make_album('james blake','james blake','11')
-print(album)	
+	album_name = input('Artist name: ')
+	if album_name == 'q':
+		break
+
+	album_info = make_album(artist_name, album_name)
+	print(album_info)
+
+#old code
+# album = make_album('tool','lateralus')
+# print(album)	
+
+# album = make_album('a perfect circle','mer de noms')
+# print(album)	
+
+# album = make_album('james blake','james blake','11')
+# print(album)	
 

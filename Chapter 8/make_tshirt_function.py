7ef656fdadef6c07ccc81bf73e412edc13339e3f
Sam Bentley <samjamesbentley@gmail.com>
2019-05-08 08:16:41 +1000

    work on chapter 8

---
+++ Chapter 8/make_tshirt_function.py
@@ -0,0 +1,5 @@
+def make_shirt(size='large',message='I Love Python'):
+	"""This function will order a t-shirt for you"""
+	print("You have ordered a " + size.lower() + " shirt.")
+	print("Your shirt has this message printed on it: " + message + "\n")
+

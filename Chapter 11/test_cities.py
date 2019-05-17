ceb9e72073b83257469f4367a9d4b0d243de45e7
Sam Bentley <samjamesbentley@gmail.com>
2019-05-17 22:03:08 +1000

    work on chatper 11

---
+++ Chapter 11/test_cities.py
@@ -0,0 +1,14 @@
+from city_country import city_country
+import unittest
+
+#city_country('melbourne','australia')
+
+class CityTestCase(unittest.TestCase):
+    """Tests for 'test_cities.py'."""
+
+    def test_city_country(self):
+        """Do locations like 'Melbourne, Australia' work?"""
+        formatted_loc = city_country('melbourne','australia')
+        self.assertEqual(formatted_loc, 'Melbourne Australia')
+
+unittest.main()

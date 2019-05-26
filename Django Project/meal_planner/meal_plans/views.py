1b2bf9e7b66c6cdac5e9b6a0c0b78e4dfdfdb224
Sam Bentley <samjamesbentley@gmail.com>
2019-05-26 16:21:46 +1000

    remove django projects

--- Django Project/meal_planner/meal_plans/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    """This is the home page for the Meal Planner"""
-    return render(request, 'meal_plans/index.html')
--- Django Project/pizzeria/piz_venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/sambentley/Desktop/pizzeria/piz_venv/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()

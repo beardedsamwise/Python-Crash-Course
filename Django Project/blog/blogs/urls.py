18c0ed00cdc4e30a80821d4c74cec39316d36616
Sam Bentley <samjamesbentley@gmail.com>
2019-05-30 07:54:41 +1000

    create basic blog post view

---
+++ Django Project/blog/blogs/urls.py
@@ -0,0 +1,11 @@
+"""defines url patterns for blogs"""
+
+from django.urls import path
+
+from . import views
+
+app_name = 'blogs'
+urlpatterns = [
+    # Home page
+    path('', views.index, name='index'),
+]
--- Django Project/blog/blogs/views.py
+++ Django Project/blog/blogs/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import BlogPost
+
+def index(request):
+    """the home page for the blog"""
+    posts = BlogPost.objects.all()
+    context = {'posts' : posts}
+    return render(request, 'blogs/index.html', context)

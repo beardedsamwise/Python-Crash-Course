20b49e31f333193678fea878ec2443d90ed3a667
Sam Bentley <samjamesbentley@gmail.com>
2019-06-10 16:21:39 +1000

    add register function

--- Django Project/blog/users/urls.py
+++ Django Project/blog/users/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     	auth_views.LoginView.as_view(template_name='users/login.html'),
     	name='login'),
 	path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+	path('regiser/', views.register, name='register'),
 ]
